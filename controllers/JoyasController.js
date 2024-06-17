95f712150484916cec04ba97a49da90a1fda6944
fernandosbenavides <fernandosbenavides@gmail.com>
2024-06-17 01:31:23 -0400

    Mejoras generales en el proyecto

--- controllers/JoyasController.js
+++ controllers/JoyasController.js
@@ -1,42 +1,70 @@
-import pool from '../db/db.js';
-// QUERY STRINGS EN LOS PARAMS
+import pool from "../db/db.js";
+
 export const getJoyas = async (req, res) => {
   try {
     const { limits, page, order_by } = req.query;
     let limit = parseInt(limits) || 10;
     let offset = (parseInt(page) - 1) * limit || 0;
-    let order = order_by ? order_by.split('_') : ['id', 'ASC'];
-    
-    const result = await pool.query(`SELECT * FROM inventario ORDER BY ${order[0]} ${order[1]} LIMIT $1 OFFSET $2`, [limit, offset]); //CONSULTA PARAMETRIZADA
-    const countResult = await pool.query('SELECT COUNT(*) FROM inventario');
-    const totalItems = parseInt(countResult.rows[0].count);
-    const totalPages = Math.ceil(totalItems / limit);
-// ESTRUCTURA HATEOAS
+    let order = order_by ? order_by.split("_") : ["id", "ASC"];
+
+    const result = await pool.query(
+      `SELECT id, nombre, COALESCE(stock, 0) AS stock FROM inventario ORDER BY ${order[0]} ${order[1]} LIMIT $1 OFFSET $2`,
+      [limit, offset]
+    );
+    //CALCULAR EL STOCK DE JOYAS
+    const stockTotalJoyas = result.rows.reduce(
+      (acc, curr) => acc + curr.stock,
+      0
+    );
+
     const data = {
-      joyas: result.rows,
-      totalItems,
-      totalPages,
-      currentPage: parseInt(page) || 1,
-      _links: {
-        self: `/joyas?limits=${limit}&page=${page}&order_by=${order_by}`,
-        next: `/joyas?limits=${limit}&page=${(parseInt(page) || 1) + 1}&order_by=${order_by}`,
-        prev: `/joyas?limits=${limit}&page=${(parseInt(page) || 1) - 1}&order_by=${order_by}`
-      }
+      joyas: result.rows.map((joya) => ({
+        nombre: joya.nombre,
+        href: `/joyas/${joya.id}`,
+      })),
+      TotalJoyas: result.rowCount, //JOYAS POR PAGINA
+      stockJoyas: stockTotalJoyas, //STOCK DE JOYAS
+      totalPages: Math.ceil(result.rowCount / limit),
+      currentPage: parseInt(page) || 1, //AGREGO HREF Y LO DEJO MAS PARECIDO A LOS REQUERIMIENTOS
     };
-// TRY Y CATCH PARA ERRORES
+
     res.json(data);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
-// FILTROS PARA EL INVENTARIO
+
+export const getJoyasById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const joyaId = parseInt(id);
+    if (isNaN(joyaId)) {
+      return res.status(400).json({ error: "ID must be an integer" }); //ALGUNAS VALIDACIONES
+    }
+
+    const query = "SELECT * FROM inventario WHERE id = $1";
+    const result = await pool.query(query, [joyaId]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Joya not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}; //AGREGO GET JOYASBYID PARA QUE AL CLICKEAR EN EL REF NO DE ERROR
+
+// FILTROS
 export const getJoyasByFilters = async (req, res) => {
   try {
     const { precio_min, precio_max, categoria, metal } = req.query;
-    let query = 'SELECT * FROM inventario WHERE 1=1';
+    let query = "SELECT * FROM inventario WHERE 1=1";
     let queryParams = [];
-    
+
     if (precio_min) {
       queryParams.push(precio_min);
       query += ` AND precio >= $${queryParams.length}`;
@@ -53,11 +81,11 @@ export const getJoyasByFilters = async (req, res) => {
       queryParams.push(metal);
       query += ` AND metal = $${queryParams.length}`;
     }
-// TRY Y CATCH PARA ERRORES
-    const result = await pool.query(query, queryParams); //CONSULTA PARAMETRIZADA
+
+    const result = await pool.query(query, queryParams);
     res.json(result.rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- routes/JoyasRoutes.js
+++ routes/JoyasRoutes.js
@@ -1,9 +1,14 @@
-import { Router } from 'express';
-import { getJoyas, getJoyasByFilters } from '../controllers/JoyasController.js';
+import { Router } from "express";
+import {
+  getJoyas,
+  getJoyasByFilters,
+  getJoyasById,
+} from "../controllers/JoyasController.js";
 
 const router = Router();
 
-router.get('/joyas', getJoyas);
-router.get('/joyas/filtros', getJoyasByFilters);
+router.get("/joyas", getJoyas);
+router.get("/joyas/filtros", getJoyasByFilters);
+router.get("/joyas/:id", getJoyasById); //AGREGO RUTA PARA LA ID
 
 export default router;
